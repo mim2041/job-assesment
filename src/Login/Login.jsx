5d3f32258674cad3ec11d70375f87759797d8e71
mim2041 <suraiyamim338@gmail.com>
2024-08-03 23:00:52 +0600

    login page responsive done

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -2,45 +2,55 @@ import login_image from '../assets/images/Rectangle 9593.png';
 import { FcGoogle } from 'react-icons/fc';
 import { FaFacebookF } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import Form from '../components/shared/LoginForm';
+import LoginForm from '../components/shared/LoginForm';
+import bg_img from '../../src/assets/images/iPhone 14 & 15 Pro Max - 6.png';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { AiOutlineEyeInvisible } from 'react-icons/ai';
+import { BsEye } from 'react-icons/bs';
+
 
 const Login = () => {
- 
+    const [ showPassword, setShowPassword ] = useState(false);
+
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { errors } 
+    } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+    };
 
     return (
         <div>
-            {/* for Desktop view */}
-            <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between lg:px-36 h-screen my-20 gap-28">
-                <div className='w-1/3'>
-                    <h1 style={{fontFamily: "Poor Richard"}} className="text-[40px] text-[#4285F3] ">LOGO</h1>
-                    <h2 className="text-[30px] font-semibold mt-4 mb-3">Log In To Your Account</h2>
-                    <p className="text-[#5C635A]">Welcome Back! Select a method to log in:</p>
-                    
-                    <div className='flex justify-between items-center my-4'>
-                        <button className="bg-gradient-to-r from-[#E4E4E4] to-white  px-8 py-3 rounded-lg mt-4 flex items-center justify-center gap-2 shadow-lg">
-                            <FcGoogle />
-                            Google
-                        </button>
-                        <button className="bg-[#156BCA] text-white px-8 py-3 rounded-lg mt-4 flex items-center justify-center gap-2 shadow-lg">
-                            <FaFacebookF />
-                            Facebook
-                        </button>
-                    </div>
-                    <div className='flex items-center justify-center gap-2 text-sm mt-4'>
-                        <hr className='w-[30%] border border-gray-200'/>
-                        <p>Or Continue with Email</p>
-                        <hr className='w-[30%] border border-gray-200'/>
-                    </div>
+            {/* for Mobile view */}
+            <div className="md:hidden " style={{backgroundImage: `url(${bg_img})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                width: '100%',
+                height: '100vh'}}
+            >
+                <div className="">
+                    <h1 style={{fontFamily: "Poor Richard"}} className="text-[40px] text-[#4285F3] text-center pt-12 pb-4">LOGO</h1>
+                    <p className="text-[#D1D1D1] text-[18px] font-[600] text-center pb-4">
+                        Sign In to view all the <br /> massage therapists
+                    </p>
 
-                    <div className='mt-6'>
-                        <Form />
-                        <div className="flex items-center justify-center">
-                        <p>
-                            Don't Have an Account? {"  "}
-                            <Link to="/sign-up" className="text-[#156BCA] font-semibold underline">Sign up</Link>
-                        </p>
-                    </div>
-                    </div>
+                    <div className={` px-4 bg-white pt-8 pb-5 rounded-t-[50px] mt-8 `} >
+                    
+                    <LoginForm />
+                </div>
+                
+                
+                </div>
+            </div>
+            {/* for Desktop view */}
+            <div className="hidden md:flex md:flex-col lg:flex-row lg:items-center lg:justify-between lg:px-36 h-screen my-20 gap-28">
+                <div className='w-1/3'>
+                    <LoginForm />
                 </div>
                 <div className="relative">
                     <div>
--- src/components/shared/LoginForm.jsx
+++ src/components/shared/LoginForm.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { AiOutlineEyeInvisible } from 'react-icons/ai';
 import { BsEye } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
@@ -19,7 +21,28 @@ const LoginForm = () => {
 
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <h1 style={{fontFamily: "Poor Richard"}} className="text-[40px] text-[#4285F3] hidden md:block">LOGO</h1>
+                    <h2 className="text-[30px] text-center md:text-left font-semibold mt-4 mb-3">Log In To Your Account</h2>
+                    <p className="text-[#5C635A] text-center md:text-left mb-8 md:mb-0">Welcome Back! Select a method to log in:</p>
+                    
+                    <div className='flex justify-center md:justify-between gap-8 items-center my-4'>
+                        <button className="bg-gradient-to-r from-[#E4E4E4] to-white  px-8 py-3 rounded-lg mt-4 flex items-center justify-center gap-2 shadow-lg text-[16px] font-[400]">
+                            <FcGoogle />
+                            Google
+                        </button>
+                        <button className="bg-[#156BCA] text-white px-8 py-3 rounded-lg mt-4 flex items-center justify-center gap-2 shadow-lg">
+                            <FaFacebookF />
+                            Facebook
+                        </button>
+                    </div>
+                    <div className='flex items-center justify-center gap-2 text-sm mt-6'>
+                        <hr className='w-[30%] border border-gray-200'/>
+                        <p>Or Continue with Email</p>
+                        <hr className='w-[30%] border border-gray-200'/>
+                    </div>
+
+                    <div className='mt-12 md:mt-6'>
+                        <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="flex flex-col gap-2 mb-4">
                             <label htmlFor="email" className="font-semibold text-md">Email</label>
                             <input type="text" id="email" placeholder="Enter your email" 
@@ -37,7 +60,7 @@ const LoginForm = () => {
                             {errors.password && <span className="text-red-600">Password is required</span>}
 
                             {/* show password toggle button */}
-                            <div className="absolute lg:right-4 top-11">
+                            <div className="absolute right-4 top-11">
                                 <button onClick={() => setShowPassword(!showPassword)}>
                                 {
                                     showPassword ? 
@@ -57,13 +80,20 @@ const LoginForm = () => {
                         </p>
                     </div>
 
-                    <div className="mt-8 mb-5 flex items-center justify-center">
+                    <div className="mt-12 md:mt-8 md:mb-5 flex items-center justify-center">
                         <button className="bg-[#4285F3] py-3 rounded-lg text-white px-24 font-semibold">
                             Sign in
                         </button>
                     </div>
                 </div>
             </form>
+                <div className="flex items-center justify-center mb-5 lg:mb-0">
+                    <p>
+                        Don't Have an Account? {"  "}
+                        <Link to="/sign-up" className="text-[#156BCA] font-semibold underline">Sign up</Link>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
